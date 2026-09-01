6893d1af995247383c1f404d6b6d20c5523fd6b7
Thierry <thierrymunderi@gmail.com>
2026-09-01 22:54:04 +0300

    feat: add vite-plugin-sitemap package with initial configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemapPlugin from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemapPlugin({
+      hostname: "https://powerwaveelectricalandsolarservices.com",
+    }),
+  ],
+});
